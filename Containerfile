97801c1566c94adb0a53f0ecda9333fc97b65a9d
qnaptail <qnaptail@proton.me>
2025-12-04 04:22:29 +0100

    Niri setup

--- Containerfile
+++ Containerfile
@@ -9,7 +9,7 @@ FROM quay.io/fedora/fedora-bootc:latest
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
 # FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
+#
 # ... and so on, here are more base images
 # Universal Blue Images: https://github.com/orgs/ublue-os/packages
 # Fedora base image: quay.io/fedora/fedora-bootc:41
--- build_files/build.sh
+++ build_files/build.sh
@@ -25,13 +25,29 @@ dnf5 -y copr enable lihaohong/yazi
 dnf5 -y copr enable varlad/zellij
 
 # INSTALL PACKAGES
-# grep -vE '^#' /usr/local/share/os-template/packages-add | xargs dnf5 -y install --allowerasing --setopt=install_weak_deps=False
-grep -vE '^#' /usr/local/share/os-template/packages-add | xargs dnf5 -y install --allowerasing
+# grep -vE '^#' /usr/local/share/os-template/packages.list | xargs dnf5 -y install --allowerasing --setopt=install_weak_deps=False
+grep -vE '^#' /usr/local/share/os-template/packages.list | xargs dnf5 -y install --allowerasing
 
-# REMOVE PACKAGES
-# grep -vE '^#' /usr/local/share/os-template/packages-remove | xargs dnf5 -y remove
-# dnf5 -y autoremove
-# dnf5 clean all
+# SWITCH KERNEL TO CACHYOS KERNEL
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+dnf5 -y install kernel-cachyos
+dnf5 -y copr disable bieszczaders/kernel-cachyos
+# Lastly if you use SELinux, you need to enable the necessary policy to be able to load kernel modules.
+setsebool -P domain_kernel_load_modules on
+
+# REMOVE UNNECESSARY AND PROBLEMATIC PACKAGES
+dnf5 -y remove \
+        PackageKit-command-not-found \
+        at \
+        iptables-services \
+        iptables-utils \
+        rsyslog \
+        dracut-config-rescue \
+        dnf-data \
+        usbmuxd
+
+dnf5 -y autoremove
 
 # Disable repos so they don't appear in final image
 dnf5 -y copr disable avengemedia/dms
@@ -39,16 +55,14 @@ dnf5 -y copr disable atim/starship
 dnf5 -y copr disable lihaohong/yazi
 dnf5 -y copr disable varlad/zellij
 
-## Hardware and system packages
-# Lenovo thinkpad fan control
-systemctl enable zcfan.service
+dnf5 clean all
 
-# Zram (ram compression to avoid swaping)
+
+# Enable Zram (ram compression to avoid swaping)
 tee /usr/lib/systemd/zram-generator.conf <<'EOF'
 [zram0]
 zram-size = min(ram / 2, 8192)
 EOF
-
 
 ## Desktop environment : Niri window manager and DankMaterial shell
 # https://github.com/YaLTeR/niri/wiki/Getting-Started
@@ -61,7 +75,7 @@ add_wants_niri() {
 add_wants_niri dms.service
 add_wants_niri udiskie.service
 # add_wants_niri swayidle.service
-# add_wants_niri foot.service
+add_wants_niri foot.service
 
 # sed -i 's|spawn-at-startup "waybar"|// spawn-at-startup "waybar"|' "/usr/share/doc/niri/default-config.kdl"
 #
@@ -79,33 +93,16 @@ systemctl enable greetd
 # https://github.com/89luca89/distrobox
 # https://docs.flatpak.org/en/latest/getting-started.html
 
-
-# Replace fedora flatpak repos with flathub
-# mkdir -p /etc/flatpak/remotes.d/
-# curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
-# rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
-# systemctl enable flatpak-add-flathub-repos.service
-
 ### Systemd units
 systemctl enable podman.socket
 systemctl enable systemd-timesyncd
 systemctl enable systemd-resolved.service
-
 systemctl mask rpm-ostree-countme.timer
+# Lenovo thinkpad fan control
+systemctl enable zcfan.service
 
 ### Troubleshooting
-
 ## The systemd-remount-fs service fails on boot because the root filesystem on an ostree system is read-only by design.
 # We can mask it to avoid harmless log errors.
 # https://gitlab.com/fedora/ostree/sig/-/issues/72
 systemctl mask systemd-remount-fs.service
-
-## The systemd-sysusers service is failing to started
-# systemd-sysusers return the following error : /etc/shadow: Group "usbmuxd" already exists.
-# seems like the group is defined twice in the /usr/lib/sysusers.d/usbmuxd.conf
-#       g usbmuxd 113
-#       u usbmuxd 113:113 "usbmuxd user"
-# we can comment the first line in the config file :
-# sed -i 's|\(g\s*usbmuxd\s*113\)|# \1|' /usr/lib/sysusers.d/usbmuxd.conf
-# we can also remove the package :
-dnf5 -y remove usbmuxd
