2d079aba1067d31c1c312807d84591be57b49127
qnaptail <qnaptail@proton.me>
2025-12-21 16:57:32 +0100

    nix test

--- build/build.sh
+++ build/build.sh
@@ -117,6 +117,7 @@ systemctl enable zcfan.service
 
 # Disabling wait-online to decrease the boot time
 systemctl disable NetworkManager-wait-online.service
+systemctl disable flatpak-add-fedora-repos.service
 
 #######################################################################
 # CONFIGURATION
@@ -144,6 +145,51 @@ systemctl mask systemd-remount-fs.service
 rm -f /usr/lib/sysusers.d/brltty.conf
 rm -f /usr/lib/sysusers.d/usbmuxd.conf
 
+#######################################################################
+# IMAGE INFOS
+#######################################################################
+
+IMAGE_VENDOR="qnaptail"
+IMAGE_NAME="tinados"
+IMAGE_PRETTY_NAME="TinadOS"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+HOME_URL="https://github.com/$IMAGE_VENDOR/$IMAGE_NAME"
+FEDORA_MAJOR_VERSION=$(awk -F= '/VERSION_ID/ {print $2}' /etc/os-release)
+BASE_IMAGE_NAME="Fedora bootc $FEDORA_MAJOR_VERSION"
+BASE_IMAGE="quay.io/fedora/fedora-bootc"
+
+IMAGE_INFO="/usr/share/$IMAGE_NAME/image-info.json"
+cat >$IMAGE_INFO <<EOF
+{
+  "image-name": "$IMAGE_NAME",
+  "image-vendor": "$IMAGE_VENDOR",
+  "image-ref": "$IMAGE_REF",
+  "image-tag":"latest",
+  "base-image-name": "$BASE_IMAGE_NAME",
+  "base-image": "$BASE_IMAGE",
+  "fedora-version": "$FEDORA_MAJOR_VERSION"
+}
+EOF
+
+echo "$IMAGE_NAME" | tee "/etc/hostname"
+
+sed -i -f - /usr/lib/os-release <<EOF
+s|^NAME=.*|NAME=\"$IMAGE_NAME\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"$IMAGE_PRETTY_NAME\"|
+s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"$IMAGE_NAME\"|
+s|^VARIANT_ID=.*|VARIANT_ID=""|
+s|^HOME_URL=.*|HOME_URL=\"${HOME_URL}\"|
+s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"${HOME_URL}/issues\"|
+s|^SUPPORT_URL=.*|SUPPORT_URL=\"${HOME_URL}/issues\"|
+s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"${HOME_URL}\"|
+s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="$IMAGE_NAME"|
+
+/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d
+EOF
+
+# Fix issues caused by ID no longer being fedora
+sed -i "s|^EFIDIR=.*|EFIDIR=\"fedora\"|" /usr/sbin/grub2-switch-to-blscfg
+
 
 #######################################################################
 # CLEANUP
@@ -158,6 +204,10 @@ dnf5 -y copr disable atim/starship
 dnf5 -y copr disable lihaohong/yazi
 dnf5 -y copr disable varlad/zellij
 dnf5 -y copr disable bieszczaders/kernel-cachyos
+
+## Remove unnecessary files
+rm -rf /usr/share/doc
+rm -rf /usr/src
 
 ## Clean all build files
 dnf5 clean all
